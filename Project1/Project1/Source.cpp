23a4f5adec187a917b9f348fbe4009121a2c0886
Nikole Garcia <wmorales83@gmail.com>
2019-05-28 08:18:20 -0500

    Probando por segunda vez

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
 
 	cout << "probando como clone, commit, push " << endl;
+	cout << " Esta es la segunda vez que edito este proyecto " << endl;
 	system("pause");
 }
